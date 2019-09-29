4531dbc5d72ed36ebbce999f522d613c67751447
Spencer Freebairn <spencerfreebairn@Spencers-Mac-mini.local>
2019-09-29 00:29:04 -0700

    add scripts to color and reset grid

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
-createGrid(16);
+init();
+
+function init() {
+    createGrid(16);
+    createListeners();
+
+    const resetButton = document.getElementById("reset-button");
+    
+    resetButton.addEventListener('click', () => {
+        let cells = document.querySelectorAll(".grid-cell");
+        cells.forEach((cell) => {
+            cell.classList.remove("invisible");
+        });
+    });
+}
 
 function createGrid(gridSize) {
     let gridArea = document.getElementById("grid-area");
@@ -7,9 +21,18 @@ function createGrid(gridSize) {
 
     for (let i = 0; i < gridSize ** 2; i++) {
         const div = document.createElement("div");
-        const para = document.createElement("p");
-
+        div.classList.add("grid-cell");
         gridArea.appendChild(div);
     }
 }
 
+function createListeners() {
+    let cells = document.querySelectorAll(".grid-cell");
+    
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', () => {
+            // cell.style.backgroundColor = "#44c2fc";
+            cell.classList.toggle("invisible");
+        });
+    });
+}
